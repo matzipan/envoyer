158926583432712e21d03672a89ed4349c34e83a
Andrei Zisu <matzipan@gmail.com>
2021-05-12 23:46:06 +0200

    Implement token receiver with actix
    This is mainly because it supports the shutting down of the server as
    well. The other library did not.

--- envoyer/src/services/token_receiver.rs
+++ envoyer/src/services/token_receiver.rs
@@ -2,28 +2,13 @@ use gtk::glib;
 
 use log::{debug, error, info};
 
-use tide::listener::ToListener;
-use tide::prelude::*;
-
 use rand::{thread_rng, Rng};
 
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::sync::mpsc;
 
 use crate::controllers::ApplicationMessage;
-
-#[derive(Clone)]
-struct TokenReceiverServerState {
-    application_message_sender: glib::Sender<ApplicationMessage>,
-}
-
-impl TokenReceiverServerState {
-    fn new(application_message_sender: glib::Sender<ApplicationMessage>) -> Self {
-        Self {
-            application_message_sender,
-        }
-    }
-}
 
 #[derive(Debug, Deserialize)]
 struct GoogleAuthorizationResponse {
@@ -32,28 +17,60 @@ struct GoogleAuthorizationResponse {
 
 pub struct TokenReceiver {
     port_number: u32,
-    binding: std::net::TcpListener,
+    server: actix_web::dev::Server,
+}
+
+pub const SUCCESS_HTML_RESPONSE: &str = "<!DOCTYPE html>
+    <html lang=\"en\">
+    <head><meta charset=\"utf-8\" />
+    <style>
+    body { font-family: sans-serif; }
+    </style>
+    <title>Authorization successful - Envoyer</title>
+    </head>
+    <body>
+    <h1>Authorization successful</h1>
+    <p>The authorization code has been successfully received. You can now close this window and switch to Envoyer.</p>
+    </body>
+    </html>";
+
+#[actix_web::get("/")]
+async fn get_token(
+    actix_web::web::Query(authorization_response): actix_web::web::Query<GoogleAuthorizationResponse>,
+    data: actix_web::web::Data<futures::channel::mpsc::Sender<String>>,
+) -> impl actix_web::Responder {
+    data.get_ref().clone().try_send(authorization_response.code).expect("BLA"); //@TODO
+
+    SUCCESS_HTML_RESPONSE
 }
 
 impl TokenReceiver {
     const PORT_RANGE: std::ops::Range<u32> = 49152..65535;
     const IP_ADDRESS: &'static str = "127.0.0.1";
 
-    pub fn new() -> Result<TokenReceiver, std::io::Error> {
-        let (port_number, binding) = Self::get_binding()?;
+    pub fn new(tx: futures::channel::mpsc::Sender<String>) -> Result<TokenReceiver, std::io::Error> {
+        let (port_number, server) = Self::get_server(tx)?;
 
-        Ok(TokenReceiver {
-            port_number,
-            binding: binding,
-        })
+        Ok(TokenReceiver { port_number, server })
     }
 
-    fn get_binding() -> Result<(u32, std::net::TcpListener), std::io::Error> {
+    fn get_server(tx: futures::channel::mpsc::Sender<String>) -> Result<(u32, actix_web::dev::Server), std::io::Error> {
         let mut port_number = Self::PORT_RANGE.start;
 
         loop {
             debug!("Attempting to bind to port {}", port_number);
-            match std::net::TcpListener::bind(format!("{}:{}", Self::IP_ADDRESS, port_number)) {
+            let tx_clone = tx.clone();
+
+            match actix_web::HttpServer::new(move || {
+                let tx_clone = tx_clone.clone();
+
+                actix_web::App::new()
+                    .app_data(actix_web::web::Data::new(tx_clone))
+                    .service(get_token)
+            })
+            .workers(1)
+            .bind(format!("{}:{}", Self::IP_ADDRESS, port_number))
+            {
                 Err(e) => match e.kind() {
                     std::io::ErrorKind::AddrInUse => {
                         debug!("Port {} is taken. Trying a new one", port_number);
@@ -67,41 +84,19 @@ impl TokenReceiver {
                 },
                 Ok(binding) => {
                     info!("Bound the token receiver to port {}", port_number);
-                    return Ok((port_number, binding));
+                    return Ok((port_number, binding.run()));
                 }
             }
         }
     }
 
-    pub async fn start(self, application_message_sender: glib::Sender<ApplicationMessage>) -> std::io::Result<()> {
-        let mut server_instance = tide::with_state(TokenReceiverServerState::new(application_message_sender));
-        server_instance.at("/").get(Self::get_token);
-
-        let mut listener = self.binding.to_listener()?;
-        listener.bind(server_instance).await?;
-        listener.accept().await?;
+    pub async fn run(self) -> std::io::Result<()> {
+        self.server.await;
 
         Ok(())
     }
 
     pub fn get_address(&self) -> String {
         format!("http://{}:{}/", Self::IP_ADDRESS, self.port_number)
-    }
-
-    async fn get_token(mut req: tide::Request<TokenReceiverServerState>) -> tide::Result {
-        let state = req.state();
-
-        let authorization_response: GoogleAuthorizationResponse = req.query()?;
-
-        state
-            .application_message_sender
-            .send(ApplicationMessage::GoogleAuthorizationCodeReceived {
-                account_name: "Gmail".to_string(), //@TODO
-                email_address: "matzipan@gmail.com".to_string(),
-                full_name: "Andrei Zisu".to_string(),
-                authorization_code: authorization_response.code,
-            });
-
-        Ok(format!("Request received. You can close this window").into())
     }
 }
