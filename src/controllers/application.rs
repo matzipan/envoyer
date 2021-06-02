51608897ba034d4ddcba714943c8414baa5a00d4
Andrei Zisu <matzipan@gmail.com>
2021-06-03 00:19:02 +0200

    Make the rust version the main version

---
+++ src/controllers/application.rs
@@ -0,0 +1,335 @@
+use gtk;
+use gtk::gio::prelude::*;
+use gtk::glib;
+
+use chrono::prelude::*;
+use diesel::prelude::*;
+use futures::prelude::*;
+
+use log::{error, info};
+
+use crate::google_oauth;
+use crate::models;
+use crate::schema;
+
+use crate::ui;
+
+use std::cell::RefCell;
+use std::env;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+
+diesel_migrations::embed_migrations!();
+
+pub enum ApplicationMessage {
+    Setup {},
+    SaveIdentity {
+        email_address: String,
+        full_name: String,
+        account_name: String,
+        identity_type: models::IdentityType,
+        gmail_access_token: String,
+        gmail_refresh_token: String,
+        expires_at: DateTime<Utc>,
+    },
+    LoadIdentities {
+        initialize: bool,
+    },
+    SetupDone {},
+    LoadFolder {
+        folder: models::Folder,
+    },
+    OpenGoogleAuthentication {
+        email_address: String,
+        full_name: String,
+        account_name: String,
+    },
+}
+
+pub struct Application {
+    main_window: Rc<RefCell<ui::Window>>,
+    welcome_dialog: Rc<RefCell<ui::WelcomeDialog>>,
+    application_message_sender: glib::Sender<ApplicationMessage>,
+    context: glib::MainContext,
+    database_connection_pool: diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::sqlite::SqliteConnection>>,
+    identities: Arc<Mutex<Vec<models::Identity>>>, //@TODO should probably be arc<identity>
+}
+
+fn get_database_path() -> Option<String> {
+    fn allow_only_absolute(path: std::path::PathBuf) -> Option<std::path::PathBuf> {
+        if path.is_absolute() {
+            Some(path)
+        } else {
+            None
+        }
+    }
+
+    env::var("XDG_DATA_HOME")
+        .ok()
+        .map(std::path::PathBuf::from)
+        .and_then(allow_only_absolute)
+        .or_else(|| {
+            env::var("HOME")
+                .ok()
+                .map(std::path::PathBuf::from)
+                .and_then(allow_only_absolute)
+                .map(|path| path.join(".local/share"))
+        })
+        .map(|path| path.join("db.sqlite"))
+        .map(|path| path.into_os_string().into_string().unwrap())
+}
+
+impl Application {
+    pub fn run() {
+        gtk::init().expect("Failed to initialize GTK Application");
+
+        let gtk_application = gtk::Application::new(Some("com.github.matzipan.envoyer"), Default::default());
+
+        gtk_application.connect_startup(|gtk_application| {
+            Application::on_startup(&gtk_application);
+        });
+
+        gtk_application.run();
+    }
+
+    fn on_startup(gtk_application: &gtk::Application) {
+        gtk_application.connect_activate(|gtk_application| {
+            let application = Application::new(gtk_application);
+
+            application.on_activate();
+        });
+    }
+
+    fn new(gtk_application: &gtk::Application) -> Application {
+        let (application_message_sender, application_message_receiver) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+        let context = glib::MainContext::default();
+
+        let database_path = get_database_path().expect("Unable to determine where to store the database");
+        info!("Using database path {}", database_path);
+
+        let database_connection_manager = diesel::r2d2::ConnectionManager::<diesel::sqlite::SqliteConnection>::new(database_path);
+        let database_connection_pool = diesel::r2d2::Pool::builder().build(database_connection_manager).unwrap();
+
+        info!("Created database connection pool");
+
+        let identities = Arc::new(Mutex::new(Vec::<models::Identity>::new()));
+
+        let application = Self {
+            main_window: Rc::new(RefCell::new(ui::Window::new(gtk_application, identities.clone()))),
+            // Ideally this dialog would be created only if account setup is
+            // needed, but to simplify reference passing right now, we're
+            // always creating it.
+            welcome_dialog: Rc::new(RefCell::new(ui::WelcomeDialog::new(application_message_sender.clone()))),
+            application_message_sender: application_message_sender,
+            context: context,
+            database_connection_pool: database_connection_pool,
+            identities: identities,
+        };
+
+        application.context.push_thread_default();
+
+        let database_connection_pool = application.database_connection_pool.clone();
+        let context_clone = application.context.clone();
+        let identities_clone = application.identities.clone();
+        let welcome_dialog = application.welcome_dialog.clone();
+        let main_window = application.main_window.clone();
+        let application_message_sender = application.application_message_sender.clone();
+        application_message_receiver.attach(None, move |msg| {
+            match msg {
+                ApplicationMessage::Setup {} => {
+                    welcome_dialog.borrow().show();
+                }
+                ApplicationMessage::SaveIdentity {
+                    email_address,
+                    full_name,
+                    account_name,
+                    identity_type,
+                    gmail_access_token: _,
+                    gmail_refresh_token,
+                    expires_at,
+                } => {
+                    info!("SaveIdentity for {}", email_address);
+
+                    let new_bare_identity = models::NewBareIdentity {
+                        email_address: &email_address,
+                        gmail_refresh_token: &gmail_refresh_token,
+                        identity_type: identity_type,
+                        expires_at: &expires_at.naive_utc(),
+                        full_name: &full_name,
+                        account_name: &account_name,
+                    };
+
+                    let connection = database_connection_pool.get().expect("Unable to acquire a database connection");
+                    diesel::insert_into(schema::identities::table)
+                        .values(&new_bare_identity)
+                        .execute(&connection)
+                        .expect("Error saving new identity");
+
+                    application_message_sender
+                        .send(ApplicationMessage::LoadIdentities { initialize: true })
+                        .expect("Unable to send application message");
+                }
+                ApplicationMessage::LoadIdentities { initialize } => {
+                    info!("LoadIdentities with initialize {}", initialize);
+
+                    let application_message_sender_clone = application_message_sender.clone();
+                    let database_connection_pool_clone = database_connection_pool.clone();
+                    let identities_clone = identities_clone.clone();
+
+                    context_clone.spawn(async move {
+                        let connection = database_connection_pool_clone
+                            .get()
+                            .expect("Unable to acquire a database connection");
+
+                        let bare_identities = schema::identities::table
+                            .load::<models::BareIdentity>(&connection)
+                            .expect("Unable to get identities from database");
+
+                        for bare_identity in bare_identities {
+                            let database_connection_pool_clone = database_connection_pool_clone.clone();
+
+                            let identity = models::Identity::new(bare_identity, database_connection_pool_clone).await;
+
+                            if initialize {
+                                identity.initialize().await.map_err(|x| error!("{}", x));
+                            }
+
+                            identities_clone.lock().expect("Unable to access identities").push(identity);
+                        }
+
+                        application_message_sender_clone
+                            .send(ApplicationMessage::SetupDone {})
+                            .expect("Unable to send application message");
+                    });
+                }
+                ApplicationMessage::SetupDone {} => {
+                    info!("SetupDone");
+
+                    for identity in &*identities_clone.lock().expect("Unable to access identities") {
+                        identity.start_session();
+                    }
+
+                    //@TODO hacky just to get things going
+                    let identity = &identities_clone.lock().expect("BLA")[0];
+
+                    let conversations = identity
+                        .get_conversations_for_folder(&identity.get_folders().unwrap().iter().find(|&x| x.folder_name == "INBOX").unwrap())
+                        .expect("BLA");
+
+                    main_window.borrow().show_conversations(conversations);
+
+                    // welcome_dialog.borrow().hide();
+                    main_window.borrow().show();
+                }
+                ApplicationMessage::LoadFolder { folder } => {
+                    //@TODO hacky just to get things going
+                    let identity = &identities_clone.lock().expect("BLA")[0];
+
+                    let conversations = identity.get_conversations_for_folder(&folder).expect("BLA");
+
+                    main_window.borrow().show_conversations(conversations);
+                }
+                ApplicationMessage::OpenGoogleAuthentication {
+                    email_address,
+                    full_name,
+                    account_name,
+                } => {
+                    let application_message_sender = application_message_sender.clone();
+
+                    let welcome_dialog_clone = welcome_dialog.clone();
+
+                    context_clone.spawn_local(
+                        google_oauth::authenticate(email_address.clone())
+                            .and_then(|authentication_result| async move {
+                                let dialog_borrow_handle = welcome_dialog_clone.borrow();
+                                dialog_borrow_handle.show_please_wait();
+                                dialog_borrow_handle.show();
+
+                                Ok(authentication_result)
+                            })
+                            .and_then(google_oauth::request_tokens)
+                            .and_then(|response_token| async move {
+                                application_message_sender
+                                    .send(ApplicationMessage::SaveIdentity {
+                                        email_address: email_address,
+                                        full_name: full_name,
+                                        identity_type: models::IdentityType::Gmail,
+                                        account_name: account_name,
+                                        gmail_access_token: response_token.access_token,
+                                        gmail_refresh_token: response_token.refresh_token,
+                                        expires_at: response_token.expires_at,
+                                    })
+                                    .map_err(|e| e.to_string())?;
+
+                                Ok(())
+                            })
+                            .map(|result| {
+                                match result {
+                                    Err(err) => {
+                                        //@TODO show in UI
+                                        error!("Unable to authenticate: {}", err);
+                                    }
+                                    _ => {}
+                                };
+                            }),
+                    );
+                }
+            }
+            // Returning false here would close the receiver and have senders
+            // fail
+            glib::Continue(true)
+        });
+
+        application.welcome_dialog.borrow().transient_for(&application.main_window.borrow());
+
+        application
+    }
+
+    fn on_activate(self) {
+        self.application_message_sender
+            .send(ApplicationMessage::Setup {})
+            .expect("Unable to send application message");
+        match self.initialize_database() {
+            Ok(_) => {
+                if self.is_account_setup_needed() {
+                    self.application_message_sender
+                        .send(ApplicationMessage::Setup {})
+                        .expect("Unable to send application message");
+                } else {
+                    self.application_message_sender
+                        .send(ApplicationMessage::LoadIdentities { initialize: false })
+                        .expect("Unable to send application message");
+                }
+            }
+            Err(e) => {
+                //@TODO show an error dialog
+                error!("Error encountered when initializing the database: {}", &e);
+            }
+        }
+    }
+
+    fn initialize_database(&self) -> Result<(), String> {
+        let connection = self.database_connection_pool.get().map_err(|e| e.to_string())?;
+
+        info!("Set up the migrations table");
+        diesel_migrations::setup_database(&connection).map_err(|e| e.to_string())?;
+
+        diesel_migrations::run_pending_migrations(&connection).map_err(|e| e.to_string())?;
+
+        Ok(())
+    }
+
+    fn is_account_setup_needed(&self) -> bool {
+        let connection = self
+            .database_connection_pool
+            .get()
+            .expect("Unable to acquire a database connection");
+
+        let identities: i64 = schema::identities::table
+            .select(diesel::dsl::count_star())
+            .first(&connection)
+            .expect("Unable to get the number of identities");
+
+        identities == 0
+    }
+}
---
+++ src/ui/window.rs
@@ -0,0 +1,314 @@
+use gtk::{gdk, glib, pango};
+
+use gtk::prelude::*;
+
+use log::info;
+
+use std::sync::{Arc, Mutex};
+
+use crate::models;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct Window {
+    pub gtk_window: gtk::ApplicationWindow,
+    threads_list_box: gtk::ListBox,
+    identities: Arc<Mutex<Vec<models::Identity>>>,
+    model: models::folder_conversations_list::model::Model,
+}
+
+pub mod folder_conversation_item {
+    use super::*;
+
+    use gtk::subclass::prelude::*;
+    // Implementation sub-module of the GObject
+    mod imp {
+        use super::*;
+
+        // The actual data structure that stores our values. This is not accessible
+        // directly from the outside.
+        pub struct FolderConversationItem {
+            pub conversation: Rc<RefCell<Option<models::Message>>>,
+        }
+
+        // Basic declaration of our type for the GObject type system
+        #[glib::object_subclass]
+        impl ObjectSubclass for FolderConversationItem {
+            const NAME: &'static str = "FolderConversationItem";
+            type Type = super::FolderConversationItem;
+            type ParentType = gtk::ListBoxRow;
+            // Called once at the very beginning of instantiation of each instance and
+            // creates the data structure that contains all our state
+            fn new() -> Self {
+                Self { conversation: Default::default() }
+            }
+        }
+        impl ObjectImpl for FolderConversationItem {}
+        impl ListBoxRowImpl for FolderConversationItem {}
+        impl WidgetImpl for FolderConversationItem {}
+    }
+
+    // The public part
+    glib::wrapper! {
+        pub struct FolderConversationItem(ObjectSubclass<imp::FolderConversationItem>) @extends gtk::ListBoxRow, gtk::Widget, @implements gtk::Buildable, gtk::Actionable;
+    }
+    impl FolderConversationItem {
+        pub fn new() -> FolderConversationItem {
+            glib::Object::new(&[]).expect("Failed to create row data")
+        }
+
+        pub fn new_with_conversation(conversation: &models::Message) -> FolderConversationItem {
+            let instance = Self::new();
+
+            let self_ = imp::FolderConversationItem::from_instance(&instance);
+            //@TODO can we get rid of this clone?
+            self_.conversation.replace(Some(conversation.clone()));
+
+            instance
+        }
+
+        pub fn get_conversation(&self) -> Rc<RefCell<Option<models::Message>>> {
+            let self_ = imp::FolderConversationItem::from_instance(self);
+            self_.conversation.clone()
+        }
+    }
+}
+
+impl Window {
+    pub fn new(application: &gtk::Application, identities: Arc<Mutex<Vec<models::Identity>>>) -> Window {
+        //@TODO set icon
+        let gtk_window = gtk::ApplicationWindow::new(application);
+        let header = gtk::HeaderBar::new();
+        header.set_title_widget(Some(&gtk::Label::new(Some("Envoyer"))));
+        gtk_window.set_titlebar(Some(&header));
+        gtk_window.set_default_size(1600, 900);
+
+        gtk::Window::set_default_icon_name("iconname");
+        let my_str = include_str!("stylesheet.css");
+        let provider = gtk::CssProvider::new();
+        provider.load_from_data(my_str.as_bytes());
+        gtk::StyleContext::add_provider_for_display(
+            &gdk::Display::default().expect("Error initializing gtk css provider."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+
+        let threads_list_box = gtk::ListBox::new();
+        threads_list_box.set_activate_on_single_click(false);
+        threads_list_box.set_selection_mode(gtk::SelectionMode::Multiple);
+
+        let folder_conversations_scroll_box = gtk::ScrolledWindow::new();
+        folder_conversations_scroll_box.set_vexpand(true);
+        folder_conversations_scroll_box.set_size_request(200, -1);
+        folder_conversations_scroll_box.set_child(Some(&threads_list_box));
+
+        let conversation_viewer_list_box = gtk::ListBox::new();
+
+        let conversation_viewer_scroll_box = gtk::ScrolledWindow::new();
+        conversation_viewer_scroll_box.set_hexpand(true);
+        conversation_viewer_scroll_box.set_hscrollbar_policy(gtk::PolicyType::Never);
+        conversation_viewer_scroll_box.set_child(Some(&conversation_viewer_list_box));
+
+        let main_grid = gtk::Grid::new();
+
+        main_grid.set_orientation(gtk::Orientation::Horizontal);
+        main_grid.attach(&folder_conversations_scroll_box, 0, 0, 1, 1);
+        main_grid.attach(&conversation_viewer_scroll_box, 1, 0, 1, 1);
+
+        gtk_window.set_child(Some(&main_grid));
+
+        let model = models::folder_conversations_list::model::Model::new();
+
+        threads_list_box.connect_row_selected(|_, row| {
+            if let Some(row) = row {
+                let row = row
+                    .downcast_ref::<folder_conversation_item::FolderConversationItem>()
+                    .expect("List box row is of wrong type");
+
+                let conversation  = row.get_conversation();
+
+                info!("{}", conversation.borrow().as_ref().expect("Model configuration invalid").subject);
+                    
+                //         assert(row is FolderConversationItem);
+                //         application.load_conversation_thread (((FolderConversationItem)
+                // row).thread);     }
+                //@TODO custom type is needed so we can get the  conversation instead of just getting the index
+                // info!("{}", row.unwrap().get_index())
+            } else {
+                //         application.unload_current_conversation_thread ();   
+            }
+        });
+
+        threads_list_box.bind_model(Some(&model), |item| {
+            let item = item
+                .downcast_ref::<models::folder_conversations_list::row_data::ConversationRowData>()
+                .expect("Row data is of wrong type");
+            
+            let conversation_rc = item.get_conversation();
+            let conversation_borrow = conversation_rc.borrow();
+            let conversation = conversation_borrow.as_ref().expect("Model contents invalid");
+
+            let box_row = folder_conversation_item::FolderConversationItem::new_with_conversation(&conversation);
+            box_row.style_context().add_class("folder_conversation_item");
+
+            let subject_label = gtk::Label::new(None);
+            subject_label.set_hexpand(true);
+            subject_label.set_halign(gtk::Align::Start);
+            subject_label.set_ellipsize(pango::EllipsizeMode::End);
+            subject_label.style_context().add_class("subject");
+            subject_label.set_xalign(0.0);
+
+            let attachment_image = gtk::Image::from_icon_name(Some("mail-attachment-symbolic"));
+            attachment_image.set_sensitive(false);
+            attachment_image.set_tooltip_text(Some("This thread contains one or more attachments"));
+
+            let top_grid = gtk::Grid::new();
+            top_grid.set_orientation(gtk::Orientation::Horizontal);
+            top_grid.set_column_spacing(3);
+
+            // unseen_dot = new Envoyer.Widgets.Main.UnseenDot ();
+            // unseen_dot.no_show_all = true;
+            // top_grid.add (unseen_dot);
+            top_grid.attach(&subject_label, 0, 0, 1, 1);
+            top_grid.attach(&attachment_image, 1, 0, 1, 1);
+
+            //@TODO make smaller star_image.
+            let star_image = gtk::Button::from_icon_name(Some("starred"));
+            star_image.style_context().add_class("star");
+            star_image.set_sensitive(true);
+            star_image.set_tooltip_text(Some("Mark this thread as starred"));
+
+            let addresses_label = gtk::Label::new(None);
+            addresses_label.set_hexpand(true);
+            addresses_label.set_halign(gtk::Align::Start);
+            addresses_label.set_ellipsize(pango::EllipsizeMode::End);
+            addresses_label.style_context().add_class("addresses");
+
+            let datetime_received_label = gtk::Label::new(None);
+            datetime_received_label.style_context().add_class("received");
+
+
+            let bottom_grid = gtk::Grid::new();
+            bottom_grid.set_orientation(gtk::Orientation::Horizontal);
+            bottom_grid.set_column_spacing(3);
+            bottom_grid.attach(&addresses_label, 0, 0, 1, 1);
+            bottom_grid.attach(&datetime_received_label, 1, 0, 1, 1);
+            bottom_grid.attach(&star_image, 2, 0, 1, 1);
+
+            let outer_grid = gtk::Grid::new();
+            outer_grid.set_orientation(gtk::Orientation::Vertical);
+            outer_grid.set_row_spacing(3);
+            outer_grid.set_margin_top(4);
+            outer_grid.set_margin_bottom(4);
+            outer_grid.set_margin_start(8);
+            outer_grid.set_margin_end(8);
+
+            outer_grid.attach(&top_grid, 0, 0, 1, 1);
+            outer_grid.attach(&bottom_grid, 0, 1, 1, 1);
+
+            box_row.set_child(Some(&outer_grid));
+
+            // Load data
+            // @TODO Currently this is done in a very naive way, to be detailed later
+            addresses_label.set_text(&conversation.from);
+            subject_label.set_text(&conversation.subject);
+
+            //@TODO implement an autoupdating timestamp
+            datetime_received_label.set_text(&conversation.get_relative_time_ago());
+
+            datetime_received_label.set_tooltip_text(Some(&conversation.time_received.to_string()));
+
+            attachment_image.hide();
+            star_image.hide();
+
+            box_row.upcast::<gtk::Widget>()
+
+            // set_swipe_icon_name ("envoyer-delete-symbolic");
+        });
+
+        Self {
+            gtk_window,
+            threads_list_box,
+            identities,
+            model,
+        }
+    }
+
+    pub fn show(&self) {
+        self.gtk_window.show();
+        self.gtk_window.present_with_time((glib::monotonic_time() / 1000) as u32);
+    }
+
+    pub fn show_conversations(&self, conversations: Vec<models::Message>) {
+        for conversation in conversations {
+            let data = models::folder_conversations_list::row_data::ConversationRowData::new();
+
+            data.set_conversation(conversation);
+
+            self.model.append(&data);
+        }
+
+        // public new void grab_focus () {
+        //     listbox.grab_focus ();
+        // }
+        // public void load_folder_handler (IFolder folder) {
+        //     current_folder = folder;
+
+        //     listbox.bind_model (folder.conversations_list_model,
+        // walk_model_items);
+        //     grab_focus ();
+        //     // @TODO listbox.select_row (item);
+        // }
+
+        //     public void show_app () {
+        //         show ();
+        //       present ();
+
+        //       folder_conversations_list.grab_focus ();
+        //   }
+
+        // let (roots, threads, envelopes) =
+        // self.identities.lock().expect("Unable to acquire identities lock")[0]
+        //     .clone()
+        //     .fetch_threads();
+
+        // let iter = roots.into_iter();
+        // for thread in iter {
+        //     let thread_node =
+        // &threads.thread_nodes()[&threads.thread_ref(thread).root()];
+        //     let root_envelope_hash = if let Some(h) =
+        // thread_node.message().or_else(|| {         if
+        // thread_node.children().is_empty() {             return None;
+        //         }
+        //         let mut iter_ptr = thread_node.children()[0];
+        //         while threads.thread_nodes()[&iter_ptr].message().is_none() {
+        //             if
+        // threads.thread_nodes()[&iter_ptr].children().is_empty() {
+        //                 return None;
+        //             }
+        //             iter_ptr =
+        // threads.thread_nodes()[&iter_ptr].children()[0];         }
+        //         threads.thread_nodes()[&iter_ptr].message()
+        //     }) {
+        //         h
+        //     } else {
+        //         continue;
+        //     };
+
+        //     let row_data = FolderConversationRowData::new(&"Subject
+        // placeholder");     unsafe {
+        //         (*row_data.as_ptr()).get_impl().subject.replace(Some(
+        //
+        // threads.thread_nodes()[&threads.thread_ref(thread).root()]
+        //                 .message()
+        //                 .as_ref()
+        //                 .map(|m|
+        // envelopes.read().unwrap()[m].subject().to_string())
+        //                 .unwrap_or_else(|| "None".to_string()),
+        //         ));
+        //     }
+
+        //     self.threads_model.append(&row_data)
+    }
+}
