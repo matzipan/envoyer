1f931c1e15e0332aee5663540f18d6c06110c65f
Andrei Zisu <matzipan@gmail.com>
2021-06-20 17:13:31 +0200

    Implement message item for passing to the model

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -75,6 +75,65 @@ pub mod folder_conversation_item {
     }
 }
 
+pub mod conversation_message_item {
+    use super::*;
+
+    use gtk::subclass::prelude::*;
+    // Implementation sub-module of the GObject
+    mod imp {
+        use super::*;
+
+        // The actual data structure that stores our values. This is not accessible
+        // directly from the outside.
+        pub struct ConversationMessageItem {
+            pub message: Rc<RefCell<Option<models::Message>>>,
+        }
+
+        // Basic declaration of our type for the GObject type system
+        #[glib::object_subclass]
+        impl ObjectSubclass for ConversationMessageItem {
+            const NAME: &'static str = "ConversationMessageItem";
+            type Type = super::ConversationMessageItem;
+            type ParentType = gtk::ListBoxRow;
+            // Called once at the very beginning of instantiation of each instance and
+            // creates the data structure that contains all our state
+            fn new() -> Self {
+                Self {
+                    message: Default::default(),
+                }
+            }
+        }
+        impl ObjectImpl for ConversationMessageItem {}
+        impl ListBoxRowImpl for ConversationMessageItem {}
+        impl WidgetImpl for ConversationMessageItem {}
+    }
+
+    // The public part
+    glib::wrapper! {
+        pub struct ConversationMessageItem(ObjectSubclass<imp::ConversationMessageItem>) @extends gtk::ListBoxRow, gtk::Widget, @implements gtk::Buildable, gtk::Actionable;
+    }
+    impl ConversationMessageItem {
+        pub fn new() -> ConversationMessageItem {
+            glib::Object::new(&[]).expect("Failed to create row data")
+        }
+
+        pub fn new_with_message(message: &models::Message) -> ConversationMessageItem {
+            let instance = Self::new();
+
+            let self_ = imp::ConversationMessageItem::from_instance(&instance);
+            //@TODO can we get rid of this clone?
+            self_.message.replace(Some(message.clone()));
+
+            instance
+        }
+
+        pub fn get_message(&self) -> Rc<RefCell<Option<models::Message>>> {
+            let self_ = imp::ConversationMessageItem::from_instance(self);
+            self_.message.clone()
+        }
+    }
+}
+
 impl Window {
     pub fn new(application: &gtk::Application, identities: Arc<Mutex<Vec<models::Identity>>>) -> Window {
         //@TODO set icon
