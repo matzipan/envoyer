6d2b58200e4f9cacc47b5dde7f93ebdf92309f24
Andrei Zisu <matzipan@gmail.com>
2021-12-13 16:39:31 +0100

    Add litehtml submodule

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     let include_path = format!("{}/include", out_path);
 
     // Build litehtml as a CMake library
-    let litehtml_build = cmake::build("litehtml");
+    let litehtml_build = cmake::build("vendor/litehtml");
 
     // Build the litehtml container as a C++ library
     cc::Build::new()
