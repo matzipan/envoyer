6f46519087b87d5d569fbb971482ae3512739cd6
Andrei Zisu <matzipan@gmail.com>
2023-09-26 22:16:52 +0200

    Move FoldersListItem to its own file

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -1,5 +1,6 @@
 pub mod dynamic_list_view;
 pub mod folder_conversation_item;
+pub mod folders_list_item;
 
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
@@ -23,64 +24,7 @@ use crate::models::folder_conversations_list::row_data::ConversationRowData;
 
 use self::dynamic_list_view::{DynamicListView, DynamicListViewStore};
 use self::folder_conversation_item::FolderConversationItem;
-
-pub mod folders_list_item {
-    use super::*;
-
-    // Implementation sub-module of the GObject
-    mod imp {
-        use super::*;
-
-        // The actual data structure that stores our values. This is not accessible
-        // directly from the outside.
-        pub struct FoldersListItem {
-            pub folder: Rc<RefCell<Option<models::Folder>>>,
-        }
-
-        // Basic declaration of our type for the GObject type system
-        #[glib::object_subclass]
-        impl ObjectSubclass for FoldersListItem {
-            const NAME: &'static str = "FoldersListItem";
-            type Type = super::FoldersListItem;
-            type ParentType = gtk::Box;
-            // Called once at the very beginning of instantiation of each instance and
-            // creates the data structure that contains all our state
-            fn new() -> Self {
-                Self {
-                    folder: Default::default(),
-                }
-            }
-        }
-        impl ObjectImpl for FoldersListItem {}
-        impl BoxImpl for FoldersListItem {}
-        impl WidgetImpl for FoldersListItem {}
-    }
-
-    // The public part
-    glib::wrapper! {
-        pub struct FoldersListItem(ObjectSubclass<imp::FoldersListItem>) @extends gtk::Widget, gtk::Box;
-    }
-    impl FoldersListItem {
-        pub fn new() -> FoldersListItem {
-            glib::Object::new::<FoldersListItem>()
-        }
-
-        pub fn new_with_folder(folder: &models::Folder) -> FoldersListItem {
-            let instance = Self::new();
-
-            let self_ = imp::FoldersListItem::from_obj(&instance);
-            //@TODO can we get rid of this clone?
-            self_.folder.replace(Some(folder.clone()));
-
-            instance
-        }
-
-        pub fn get_folder(&self) -> Rc<RefCell<Option<models::Folder>>> {
-            let self_ = imp::FoldersListItem::from_obj(self);
-            self_.folder.clone()
-        }
-    }
-}
+use self::folders_list_item::FoldersListItem;
 
 pub mod message_view {
     use super::*;
