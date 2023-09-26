60b9d0e97af0dc0cf51e0a786113be9179b11e23
Andrei Zisu <matzipan@gmail.com>
2023-09-26 22:25:57 +0200

    Move MessageView to its own file

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -1,6 +1,7 @@
 pub mod dynamic_list_view;
 pub mod folder_conversation_item;
 pub mod folders_list_item;
+pub mod message_view;
 
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
@@ -25,135 +26,7 @@ use crate::models::folder_conversations_list::row_data::ConversationRowData;
 use self::dynamic_list_view::{DynamicListView, DynamicListViewStore};
 use self::folder_conversation_item::FolderConversationItem;
 use self::folders_list_item::FoldersListItem;
-
-pub mod message_view {
-    use super::*;
-
-    // Implementation sub-module of the GObject
-    mod imp {
-        use crate::litehtml_callbacks;
-
-        use super::*;
-
-        // The actual data structure that stores our values. This is not accessible
-        // directly from the outside.
-        pub struct MessageView {
-            pub litehtml_callbacks: Rc<RefCell<litehtml_callbacks::Callbacks>>,
-            pub litehtml_context: Rc<RefCell<*mut core::ffi::c_void>>,
-            pub loaded: Rc<RefCell<bool>>,
-        }
-
-        // Basic declaration of our type for the GObject type system
-        #[glib::object_subclass]
-        impl ObjectSubclass for MessageView {
-            const NAME: &'static str = "MessageView";
-            type Type = super::MessageView;
-            type ParentType = gtk::Widget;
-            // Called once at the very beginning of instantiation of each instance and
-            // creates the data structure that contains all our state
-            fn new() -> Self {
-                Self {
-                    litehtml_callbacks: Rc::new(RefCell::new(litehtml_callbacks::Callbacks::new())),
-                    litehtml_context: Rc::new(RefCell::new(0 as *mut core::ffi::c_void)),
-                    loaded: Rc::new(RefCell::new(false)),
-                }
-            }
-        }
-
-        impl ObjectImpl for MessageView {
-            fn constructed(&self) {
-                self.parent_constructed();
-
-                let obj = self.obj();
-                obj.set_vexpand(true);
-            }
-        }
-
-        impl WidgetImpl for MessageView {
-            fn snapshot(&self, snapshot: &gtk::Snapshot) {
-                if !*self.loaded.borrow() {
-                    return;
-                }
-
-                let litehtml_context = self.litehtml_context.borrow_mut();
-
-                let mut callbacks = self.litehtml_callbacks.borrow_mut();
-                callbacks.clear_nodes();
-
-                unsafe {
-                    bindings::setup::draw(*litehtml_context);
-                }
-
-                let nodes = callbacks.nodes();
-
-                let container_node = gsk::ContainerNode::new(&nodes);
-
-                snapshot.append_node(&container_node);
-            }
-
-            fn request_mode(&self) -> gtk::SizeRequestMode {
-                gtk::SizeRequestMode::HeightForWidth
-            }
-
-            fn measure(&self, orientation: gtk::Orientation, for_size: i32) -> (i32, i32, i32, i32) {
-                let litehtml_context = self.litehtml_context.borrow_mut();
-
-                match orientation {
-                    gtk::Orientation::Horizontal => (0, 0, -1, -1),
-                    gtk::Orientation::Vertical => {
-                        let height = unsafe { bindings::setup::render(*litehtml_context, for_size * pango::SCALE) } / pango::SCALE;
-
-                        (height, height, -1, -1)
-                    }
-                    _ => unimplemented!(),
-                }
-            }
-
-            fn size_allocate(&self, width: i32, height: i32, baseline: i32) {
-                let litehtml_context = self.litehtml_context.borrow_mut();
-
-                unsafe { bindings::setup::render(*litehtml_context, width * pango::SCALE) };
-
-                let obj = self.obj();
-
-                obj.queue_draw();
-            }
-        }
-    }
-
-    // The public part
-    glib::wrapper! {
-        pub struct MessageView(ObjectSubclass<imp::MessageView>) @extends gtk::Widget, @implements gtk::Buildable, gtk::Actionable;
-    }
-    impl MessageView {
-        pub fn new() -> MessageView {
-            glib::Object::new::<MessageView>()
-        }
-
-        pub fn load_content(&self, content: &String) {
-            let self_ = imp::MessageView::from_obj(self);
-
-            let master_stylesheet = include_str!("../ui/webview_stylesheet.css");
-            let master_stylesheet = CString::new(master_stylesheet).expect("Could not build master stylesheet CString");
-
-            let s = CString::new(&**content).expect("CString::new failed");
-
-            let mut litehtml_context = self_.litehtml_context.borrow_mut();
-
-            unsafe {
-                *litehtml_context = bindings::setup::setup_litehtml(
-                    master_stylesheet.as_ptr(),
-                    s.as_ptr(),
-                    RefCell::as_ptr(&self_.litehtml_callbacks) as *mut core::ffi::c_void,
-                );
-            }
-
-            *self_.loaded.borrow_mut() = true;
-
-            self.queue_resize();
-        }
-    }
-}
+use self::message_view::MessageView;
 
 pub mod conversation_message_item {
     use super::*;
@@ -479,7 +352,7 @@ mod imp {
                     let attachments_list = gtk::Grid::new();
                     attachments_list.set_orientation(gtk::Orientation::Vertical);
 
-                    let view = message_view::MessageView::new();
+                    let view = MessageView::new();
 
                     let grid = gtk::Grid::new();
                     grid.set_orientation(gtk::Orientation::Vertical);
